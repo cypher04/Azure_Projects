007a8e91bf877e2cb84219974496178436887aa7
Seyi Abiodun <cypher04>
2025-12-22 22:32:35 +0000

    added internet query option to app insights

--- blue_green_deployment_with_slots_tm/modules/monitoring/main.tf
+++ blue_green_deployment_with_slots_tm/modules/monitoring/main.tf
@@ -14,6 +14,7 @@ resource "azurerm_application_insights" "app_insights" {
     resource_group_name = var.resource_group_name
     application_type    = "web"
     internet_ingestion_enabled = false
+    internet_query_enabled = false
 }
 
 
