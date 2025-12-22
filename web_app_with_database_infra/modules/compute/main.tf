e5527bdfb29e7a3c5685c17cf2f2edd9041641d0
Seyi Abiodun <cypher04>
2025-12-22 22:36:04 +0000

    added client certificate mode to linux app

--- web_app_with_database_infra/modules/compute/main.tf
+++ web_app_with_database_infra/modules/compute/main.tf
@@ -25,6 +25,7 @@ resource "azurerm_linux_web_app" "liweb" {
     resource_group_name = var.resource_group_name
     service_plan_id     = azurerm_app_service_plan.main.id
     client_certificate_enabled = true
+    client_certificate_mode = "Required"
     identity {
         type = "SystemAssigned"
     }
