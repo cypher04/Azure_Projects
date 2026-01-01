7d124af72b1b4d9a58e4ae47d981e1a90b38982a
Seyi Abiodun <cypher04>
2026-01-01 21:37:29 +0000

    added monitoring module to the infra

--- resource_config_with_ansible/modules/compute/output.tf
+++ resource_config_with_ansible/modules/compute/output.tf
@@ -2,10 +2,6 @@ output "subnet_ids" {
   value = var.subnet_ids
 }
 
-output "virtual_machine_ids" {
-  value = azurerm_linux_virtual_machine.vm.*.id
-}
-
 output "virtual_machine_private_ip_addresses" {
   value = azurerm_linux_virtual_machine.vm.*.private_ip_address
 }
